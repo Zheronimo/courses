37727e5d0685a3186f44d622907200fe15ba3bfc
Zheronimo <kirichenko.e.o@gmail.com>
2017-06-18 20:40:08 +0300

    header end

--- js/common.js
+++ js/common.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
+	// Подгоняем высоту фона под размер экрана
+	function wResize() {
+		$(".header").css("min-height", $(window).height());
+	};
+	wResize();
+	$(window).resize(function() {
+		wResize();
+	});
+	// Tab
+	$('.contacts .tab').click(function() {
+		$('.contacts .tab').removeClass('active').eq($(this).index()).addClass('active');
+		$('.contacts .tab_item').hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass('active');
 
+	$('.header_discounts .tab').click(function() {
+		$('.header_discounts .tab').removeClass('active').eq($(this).index()).addClass('active');
+		$('.header_discounts .tab_item').hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass('active');
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
 		ga("send", "event", "goal", "goal");
